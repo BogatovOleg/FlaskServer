21daa0a72738d0c463946886dc632468bc2b6054
bogat <bogatov.omsk55@gmail.com>
2021-05-30 16:55:52 +0600

    test navigation commands

--- application.py
+++ application.py
@@ -22,11 +22,39 @@ def main():
 
     req = request.json
     if req["session"]["new"]:
-        response["response"]["text"] = "Привет! Как твои дела? Как отметил новый год?"
+        response["response"]["text"] = "Привет! Меня зовут Алиса, я твой Голосовой помощник для управления страницей браузера. Чтобы узнать список моих команд, скажите слово 'Команды'."
     else:
-        if req["request"]["original_utterance"].lower() in ["хорошо", "отлично"]:
-            response["response"]["text"] = "Супер! Я за вас рада!"
-        elif req["request"]["original_utterance"].lower() in ["плохо", "скучно"]:
-            response["response"]["text"] = "Это печально... нужно было позвать меня!"
+        if req["request"]["original_utterance"].lower() in ["команды", "список команд"]:
+            response["response"]["text"] = "Список команд для работы с браузером(можно использовать синонимы):/n" \
+                                       "Для навигации по странице: 1)Наверх 2)Выше 3)Чуть выше. По аналогии с перемещением вниз./n" \
+                                           "Для перехода между страницами: 1)Вперед 2)Назад./n" \
+                                           "Для закрытия браузера: 1)Закрыть браузер."
+        elif req["request"]["original_utterance"].lower() in ["в начало страницы", "начало", "в самое начало", "наверх"]:
+            response["response"]["text"] = "func_up_full"
+
+        elif req["request"]["original_utterance"].lower() in ["в самый низ", "конец", "в самый конец",
+                                                              "вниз"]:
+            response["response"]["text"] = "func_down_full"
+
+        elif req["request"]["original_utterance"].lower() in ["выше"]:
+            response["response"]["text"] = "func_up_normal"
+
+        elif req["request"]["original_utterance"].lower() in ["ниже"]:
+            response["response"]["text"] = "func_down_normal"
+
+        elif req["request"]["original_utterance"].lower() in ["чуть выше", "немного выше"]:
+            response["response"]["text"] = "func_up_abit"
+
+        elif req["request"]["original_utterance"].lower() in ["чуть ниже", "немного ниже"]:
+            response["response"]["text"] = "func_down_abit"
+
+        elif req["request"]["original_utterance"].lower() in ["закрыть браузер"]:
+            response["response"]["text"] = "func_exit"
+
+        elif req["request"]["original_utterance"].lower() in ["вперед"]:
+            response["response"]["text"] = "func_forward"
+
+        elif req["request"]["original_utterance"].lower() in ["назад"]:
+            response["response"]["text"] = "func_back"
 
     return json.dumps(response)
