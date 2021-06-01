1371e5036119624334b4ddfbb4cf6922db6bf5fc
bogat <bogatov.omsk55@gmail.com>
2021-06-02 01:23:35 +0600

    old struct

--- application.py
+++ application.py
@@ -17,63 +17,56 @@ logging.basicConfig(level=logging.DEBUG)
 sessionStorage = {}
 
 name = ''
-
-
-@application.route('/post', methods=['POST'])
+@application.route("/", methods=["POST"])
 def main():
-    ## Создаем ответ
+    logging.info('Request: %r', request.json)
+
     response = {
-        'session': request.json['session'],
-        'version': request.json['version'],
-        'response': {
-            'end_session': False
+        "version": request.json["version"],
+        "session": request.json["session"],
+        "response": {
+            "end_session": False
         }
     }
-    ## Заполняем необходимую информацию
-    handle_dialog(response, request.json)
-    return json.dumps(response)
 
-
-def handle_dialog(res, req):
+    req = request.json
     if req["session"]["new"]:
-        res["response"][
+        response["response"][
             "text"] = "Привет! Меня зовут Алиса, я твой Голосовой помощник для управления страницей браузера.\nЧтобы узнать список моих команд, скажите слово 'Команды'."
     else:
         if req["request"]["original_utterance"].lower() in ["команды", "список команд"]:
-            res["response"]["text"] = "Список команд для работы с браузером(можно использовать синонимы):\n" \
-                                      "Для навигации по странице: 1)Наверх 2)Выше 3)Чуть выше. По аналогии с перемещением вниз.\n" \
-                                      "Для перехода между страницами: 1)Вперед 2)Назад.\n" \
-                                      "Для закрытия браузера: 1)Закрыть браузер."
+            response["response"]["text"] = "Список команд для работы с браузером(можно использовать синонимы):\n" \
+                                           "Для навигации по странице: 1)Наверх 2)Выше 3)Чуть выше. По аналогии с перемещением вниз.\n" \
+                                           "Для перехода между страницами: 1)Вперед 2)Назад.\n" \
+                                           "Для закрытия браузера: 1)Закрыть браузер."
         elif req["request"]["original_utterance"].lower() in ["в начало страницы", "начало", "в самое начало",
                                                               "наверх"]:
-            res["response"]["text"] = "func_up_full"
+            response["response"]["text"] = "func_up_full"
 
         elif req["request"]["original_utterance"].lower() in ["в самый низ", "конец", "в самый конец",
                                                               "вниз"]:
-            res["response"]["text"] = "func_down_full"
+            response["response"]["text"] = "func_down_full"
 
         elif req["request"]["original_utterance"].lower() in ["выше"]:
-            res["response"]["text"] = "func_up_normal"
+            response["response"]["text"] = "func_up_normal"
             sqs_func('func_up_normal')
 
         elif req["request"]["original_utterance"].lower() in ["ниже"]:
-            res["response"]["text"] = "func_down_normal"
+            response["response"]["text"] = "func_down_normal"
 
         elif req["request"]["original_utterance"].lower() in ["чуть выше", "немного выше"]:
-            res["response"]["text"] = "func_up_abit"
+            response["response"]["text"] = "func_up_abit"
 
         elif req["request"]["original_utterance"].lower() in ["чуть ниже", "немного ниже"]:
-            res["response"]["text"] = "func_down_abit"
+            response["response"]["text"] = "func_down_abit"
 
         elif req["request"]["original_utterance"].lower() in ["закрыть браузер"]:
-            res["response"]["text"] = "func_exit"
+            response["response"]["text"] = "func_exit"
 
         elif req["request"]["original_utterance"].lower() in ["вперед"]:
-            res["response"]["text"] = "func_forward"
+            response["response"]["text"] = "func_forward"
 
         elif req["request"]["original_utterance"].lower() in ["назад"]:
-            res["response"]["text"] = "func_back"
+            response["response"]["text"] = "func_back"
 
-
-if __name__ == '__main__':
-    application.run()
+    return json.dumps(response)
